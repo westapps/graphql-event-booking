eb2dd2ea5bf6436a41c4bd6f96f99d0d88a2c920
Simon <greenspanx@hotmail.com>
2019-09-16 12:36:48 +0800

    graphqlHttp

--- app.js
+++ app.js
@@ -2,15 +2,43 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 
 const util = require('./util/util');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.get('/', (req, res, next) => {
-  res.end('Hello world!');
-});
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    },
+    type RootMutation {
+      createEvent(name: String): String
+    },
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['Romantic Cooking', 'Sailing', 'all-night Coding'];
+    },
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName;
+    },
+  },
+  graphiql: true
+}));
+
+// app.get('/', (req, res, next) => {
+//   res.end('Hello world!');
+// });
 
 // log errors
 app.use(util.logErrors);
