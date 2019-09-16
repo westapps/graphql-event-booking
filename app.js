9be52bbd942e3dc9859e112b9e332fbe2f6976f3
Simon <greenspanx@hotmail.com>
2019-09-16 16:43:08 +0800

    save to mongodb

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 
 const util = require('./util/util');
@@ -50,16 +53,31 @@ app.use('/graphql', graphqlHttp({
       return events;
     },
     createEvent: (args) => {
-      const event = {
-        _id: Math.random().toString(),
+      // const event = {
+      //   _id: Math.random().toString(),
+      //   title: args.eventInput.title,
+      //   description: args.eventInput.description,
+      //   price: +args.eventInput.price,
+      //   date: args.eventInput.date
+      // };
+      // console.log(args);
+      const event = new Event({
         title: args.eventInput.title,
         description: args.eventInput.description,
         price: +args.eventInput.price,
-        date: args.eventInput.date
-      };
-      console.log(args);
-      events.push(event);
-      return event;
+        date: new Date(args.eventInput.date)
+      });
+      // return a promise 
+      return event.save()
+      .then(result => {
+        console.log(result);
+        return {...result._doc};
+      })
+      .catch(err => {
+        console.log(err);
+        throw err;
+      });
+
     }
   },
   graphiql: true
