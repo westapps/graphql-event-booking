e58a63b851d93fe7e4480f8a78a9406e1fe596f2
Simon <greenspanx@hotmail.com>
2019-09-22 14:38:12 +0800

    cancelBooking

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -153,9 +153,9 @@ module.exports = {
     try{
       const booking = await Booking.findById(args.bookingId).populate('event');
       const event = {
-        ...booking._doc,
+        ...booking.event._doc,
         _id: booking.event.id,
-        creator: user.bind(this, booking.creator)
+        creator: user.bind(this, booking.event._doc.creator)
       };
       await Booking.deleteOne({_id: args.bookingId});
       return event;
